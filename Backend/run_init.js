e564b3505265c211b549f988a3f7982feae229f2
Darlington <darlington@example.com>
2026-06-05 16:02:59 +0000

    build: untrack Backend folder and ignore it in .gitignore

--- Backend/run_init.js
+++
@@ -1,9 +0,0 @@
-const initializeDatabase = require('./src/initDb');
-
-async function runInit() {
-  console.log('Running database initialization...');
-  await initializeDatabase();
-  process.exit();
-}
-
-runInit();
